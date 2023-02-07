914a5fd337a1a4222d3ce7d414787491351befa1
Matheus <matheus_cs.nf@hotmail.com>
2023-02-07 10:02:39 -0300

    adicionando .env ao gitignore

--- src/services/api/api.jsx
+++ src/services/api/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getUserLocalStorage } from "../../utils/localStorage";
 
 const Api = axios.create({
-  baseURL: 'http://localhost:3001/' || process.env.REACT_APP_URL_HOST || 'http://localhost:3001/' , headers: { "Content-Type": "application/json", Authorization: getUserLocalStorage()?.token || null }
+  baseURL: process.env.REACT_APP_URL_HOST || 'http://localhost:3001/' , headers: { "Content-Type": "application/json", Authorization: getUserLocalStorage()?.token || null }
 })
 
 
