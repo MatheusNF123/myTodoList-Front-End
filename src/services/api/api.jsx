d9e5dadc34da0c202a1a1d425d1144fce55c73f1
Matheus <matheus_cs.nf@hotmail.com>
2023-02-07 13:42:19 -0300

    adicionando interceptador do axios para enviar token nas requisiçoes

--- src/services/api/api.jsx
+++ src/services/api/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getUserLocalStorage } from "../../utils/localStorage";
 
 const Api = axios.create({
-  baseURL: process.env.REACT_APP_URL_HOST || 'http://localhost:3001/' , headers: { "Content-Type": "application/json", Authorization: getUserLocalStorage()?.token || null }
+  baseURL: process.env.REACT_APP_URL_HOST || 'http://localhost:3001/' , headers: { "Content-Type": "application/json", /* Authorization: getUserLocalStorage()?.token || null */ }
 })
 
 
@@ -17,6 +17,21 @@ Api.interceptors.response.use(function (response) {
   console.log('error');
   return Promise.reject(error);
 });
+
+Api.interceptors.request.use(
+  config => {
+    // Get the token from local storage or any other means
+    const token = getUserLocalStorage()?.token || null
+
+    // If there's a token, add it to the Authorization header
+    if (token) {
+      config.headers.Authorization = token;
+    }
+
+    return config;
+  },
+  error => Promise.reject(error)
+);
 
 
 export {Api}
--- src/services/user/authUser.js
+++ src/services/user/authUser.js
@@ -1,16 +1,11 @@
 import { Api } from "../api/api";
 
 const authTokenUser = async () => {
-  // try {
+ 
     const {data} = await Api.get("auth");
-    // if(data){
+
       return data;
-  //   }
-  //   return new Error('Usuário não autenticado.');
-  // } catch (error) {
-  //   return null
-  //   // return new Error(error.message || 'Usuário não autenticado.');
-  // }
+
 };
 
 export default authTokenUser;
